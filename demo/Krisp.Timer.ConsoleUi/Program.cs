60ffc3ecedb32d3905c91727725f989b56e203b6
MayisH-dev <harutyunyanmayis+github@gmail.com>
2021-07-13 10:33:12 -0700

    Add console demo

---
+++ demo/Krisp.Timer.ConsoleUi/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Krisp.Timer;
+
+ITimer timer = new Krisp.Timer.Timer();
+Enumerable.Range(1,20)
+    .AsParallel()
+    .WithExecutionMode(ParallelExecutionMode.ForceParallelism)
+    .ForAll(num =>
+    {
+        int iter = 1;
+        timer.Start(_ =>
+            Console.WriteLine(
+                "Now: {3}, Scheduled callback Id: {0,2}, Printing every {1,3} milliseconds, print count: {2,6}",
+                num,
+                num * 5,
+                iter++,
+                DateTime.Now.Ticks),
+            TimeSpan.FromMilliseconds(num * 5),
+            ITimer.UnlimitedRecurrence);
+    });
+
+
+await Task.Delay(100000);
