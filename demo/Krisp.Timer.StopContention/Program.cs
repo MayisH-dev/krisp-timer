3ad0b44330caf15277297ae0453a86a43242dbfa
MayisH-dev <harutyunyanmayis+github@gmail.com>
2021-07-14 01:51:10 -0700

    Increase the number of scheduled tasks in demo

--- demo/Krisp.Timer.StopContention/Program.cs
+++ demo/Krisp.Timer.StopContention/Program.cs
@@ -6,13 +6,14 @@ using Krisp.Timer;
 // Showcases the non deterministic nature of Timer.Stop()
 // In a highly concurrent system, the upside of synchronizing this operation will not have much upside anyways
 // We just need Timer's underlying cache of request tokens to always stay consistent with the callbacks that were not cancelled by Stop()
+// Thus, the outcome of this program greatly depends on the PLINQ partitioner(the size of the chunks which are scheduled for execution on the same OS thread)
 ITimer timer = new Krisp.Timer.Timer();
 
-ParallelEnumerable.Range(1, 10)
+ParallelEnumerable.Range(1, 50)
     .WithExecutionMode(ParallelExecutionMode.ForceParallelism)
     .ForAll(
         callbackId => {
-            if(callbackId is 5)
+            if(callbackId is 25)
                 timer.Stop();
             else
                 timer.Start(
